053e8a66a666b80e56bbdd1998e831d63a15db8f
shiqstone <shiqstone@gmail.com>
2017-12-28 16:10:52 +0800

    inotify + rsync 方式同步目录

---
+++ inotify_rsync.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+# make sure user login from src server to dest server by ssh-key
+
+src=/home/user/source # local base path
+dest=/home/user/dest  # remote destinate path
+user=sync_user
+opt=''
+SERVERS='10.0.0.1 10.0.0.2'
+
+sync_one()
+{
+  echo "Pushing "$1
+  #echo "/usr/bin/rsync -av $opt $src ${user}@$1:$dest"
+  /usr/bin/rsync -av $opt $src ${user}@$1:$dest
+}
+
+sync_all()
+{
+  #. /home/user/servers.sh 
+  for S in $SERVERS
+  do
+      sync_one $S
+  done
+}
+
+inotify_file()
+{
+  cd $src
+  /usr/bin/inotifywait -mrq --format '%Xe %w%f' -e modify,create,delete,attrib,close_write,move ./ | while read file
+  do
+    INO_EVENT=$(echo $file | awk '{print $1}')      
+    INO_FILE=$(echo $file | awk '{print $2}')  
+    if [[ $INO_EVENT =~ 'CREATE' ]] || [[ $INO_EVENT =~ 'MODIFY' ]] || [[ $INO_EVENT =~ 'CLOSE_WRITE' ]] || [[ $INO_EVENT =~ 'MOVED_TO' ]]
+    then
+      opt=''
+      sync_all
+    fi
+    if [[ $INO_EVENT =~ 'DELETE' ]] || [[ $INO_EVENT =~ 'MOVED_FROM' ]]
+    then
+        opt=--delete
+        sync_all
+    fi
+    if [[ $INO_EVENT =~ 'ATTRIB' ]]
+    then
+      if [ ! -d "$INO_FILE" ]
+      then
+        opt=''
+        sync_all
+      fi
+    fi
+  done
+}
+
+process=$0
+pid=$$
+cur=`ps -ef |grep $process |grep -v grep |grep -v $pid |grep -v $PPID`
+#echo $cur
+if [ "_$cur" = "_" ]; then
+  echo "start running"
+  inotify_file
+else
+  echo "process has running"
+fi 
+exit 1
